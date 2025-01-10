74788b9091797d01945ab1e4404daa5616797138
Pi Lanningham <pi@sundae.fi>
2025-01-10 12:13:50 -0500

    Move internals into submodule
    This should make conditional compilation easier, since we can enable/disable the module as a whole

--- crates/amaru/src/bin/amaru/metrics.rs
+++ crates/amaru/src/bin/amaru/metrics.rs
@@ -1,12 +1,9 @@
-use opentelemetry::{
-    metrics::{Counter, Gauge, MeterProvider},
-    KeyValue,
-};
 use opentelemetry_sdk::metrics::SdkMeterProvider;
 use tokio::task::JoinHandle;
 
 #[cfg(not(windows))]
 pub fn track_system_metrics(metrics: SdkMeterProvider) -> JoinHandle<()> {
+    use internals::*;
     use std::time::Duration;
     use tracing::warn;
     tokio::spawn(async move {
@@ -42,89 +39,94 @@ pub fn track_system_metrics(_metrics: SdkMeterProvider) -> JoinHandle<()> {
     tokio::spawn(async {})
 }
 
-struct SystemCounters {
-    total_memory: Gauge<u64>,
-    free_memory: Gauge<u64>,
-    cpu_load: Gauge<f64>,
-    user_time: Counter<u64>,
-}
-
 #[cfg(not(windows))]
-#[derive(Debug)]
-struct Reading {
-    memory: sys_metrics::memory::Memory,
-    cpu: sys_metrics::cpu::CpuTimes,
-    load: sys_metrics::cpu::LoadAvg,
-}
-
-fn make_system_counters(metrics: SdkMeterProvider) -> SystemCounters {
-    // TODO: standardize with the Haskell node somehow?
-    let meter = metrics.meter("system");
-    let total_memory = meter
-        .u64_gauge("memory.limit")
-        .with_description("The total system memory, updated once per second")
-        .with_unit("MB")
-        .build();
-
-    let free_memory = meter
-        .u64_gauge("memory.usage")
-        .with_description("The free system memory, measured once per second")
-        .with_unit("MB")
-        .build();
-
-    let cpu_load = meter
-        .f64_gauge("cpu.utilization")
-        .with_description("the 1m average load, measured once per second")
-        .build();
-
-    let user_time = meter
-        .u64_counter("cpu.time")
-        .with_description("the total cpu time spent in user processes")
-        .with_unit("ms")
-        .build();
-
-    SystemCounters {
-        total_memory,
-        free_memory,
-        cpu_load,
-        user_time,
-    }
-}
-
-#[cfg(not(windows))]
-fn get_reading() -> miette::Result<Reading> {
+mod internals {
     use miette::IntoDiagnostic;
+    use opentelemetry::{
+        metrics::{Counter, Gauge, MeterProvider},
+        KeyValue,
+    };
+    use opentelemetry_sdk::metrics::SdkMeterProvider;
     use sys_metrics::*;
-    let memory = memory::get_memory().into_diagnostic()?;
-    let cpu = cpu::get_cputimes().into_diagnostic()?;
-    let load = cpu::get_loadavg().into_diagnostic()?;
 
-    Ok(Reading { memory, cpu, load })
-}
+    pub struct SystemCounters {
+        total_memory: Gauge<u64>,
+        free_memory: Gauge<u64>,
+        cpu_load: Gauge<f64>,
+        user_time: Counter<u64>,
+    }
 
-#[cfg(not(windows))]
-fn record_system_metrics(reading: Reading, counters: &SystemCounters) {
-    counters.total_memory.record(reading.memory.total, &[]);
-    counters.free_memory.record(reading.memory.free, &[]);
-    counters.cpu_load.record(reading.load.one, &[]);
-    counters
-        .user_time
-        .add(reading.cpu.user, &[KeyValue::new("state", "user")]);
-    counters
-        .user_time
-        .add(reading.cpu.system, &[KeyValue::new("state", "system")]);
-}
+    #[derive(Debug)]
+    pub struct Reading {
+        memory: sys_metrics::memory::Memory,
+        cpu: sys_metrics::cpu::CpuTimes,
+        load: sys_metrics::cpu::LoadAvg,
+    }
 
-#[cfg(all(test, not(windows)))]
-mod tests {
-    use super::*;
+    pub fn make_system_counters(metrics: SdkMeterProvider) -> SystemCounters {
+        // TODO: standardize with the Haskell node somehow?
+        let meter = metrics.meter("system");
+        let total_memory = meter
+            .u64_gauge("memory.limit")
+            .with_description("The total system memory, updated once per second")
+            .with_unit("MB")
+            .build();
 
-    #[test]
-    fn can_read_system_metrics() {
-        let reading = get_reading().expect("failed to read system metrics");
-        assert!(reading.memory.free > 0, "failed to read free memory");
-        assert!(reading.memory.total > 0, "failed to read total memory");
-        assert!(reading.cpu.user > 0, "failed to read user cpu time");
-        assert!(reading.load.one > 0.0, "failed to read cpu load average");
+        let free_memory = meter
+            .u64_gauge("memory.usage")
+            .with_description("The free system memory, measured once per second")
+            .with_unit("MB")
+            .build();
+
+        let cpu_load = meter
+            .f64_gauge("cpu.utilization")
+            .with_description("the 1m average load, measured once per second")
+            .build();
+
+        let user_time = meter
+            .u64_counter("cpu.time")
+            .with_description("the total cpu time spent in user processes")
+            .with_unit("ms")
+            .build();
+
+        SystemCounters {
+            total_memory,
+            free_memory,
+            cpu_load,
+            user_time,
+        }
+    }
+
+    pub fn get_reading() -> miette::Result<Reading> {
+        let memory = memory::get_memory().into_diagnostic()?;
+        let cpu = cpu::get_cputimes().into_diagnostic()?;
+        let load = cpu::get_loadavg().into_diagnostic()?;
+
+        Ok(Reading { memory, cpu, load })
+    }
+    pub fn record_system_metrics(reading: Reading, counters: &SystemCounters) {
+        counters.total_memory.record(reading.memory.total, &[]);
+        counters.free_memory.record(reading.memory.free, &[]);
+        counters.cpu_load.record(reading.load.one, &[]);
+        counters
+            .user_time
+            .add(reading.cpu.user, &[KeyValue::new("state", "user")]);
+        counters
+            .user_time
+            .add(reading.cpu.system, &[KeyValue::new("state", "system")]);
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn can_read_system_metrics() {
+            let reading = get_reading().expect("failed to read system metrics");
+            assert!(reading.memory.free > 0, "failed to read free memory");
+            assert!(reading.memory.total > 0, "failed to read total memory");
+            assert!(reading.cpu.user > 0, "failed to read user cpu time");
+            assert!(reading.load.one > 0.0, "failed to read cpu load average");
+        }
     }
 }
