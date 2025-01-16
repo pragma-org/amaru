ec47961d6380949f735b6aec8ebed971089c6b24
KtorZ <matthias.benkort@gmail.com>
2025-01-16 11:50:20 +0100

    chore: rework and align ledger.db arg passing across commands
      - Use module constants to avoid repetitions;
      - Align option names across commands when they refer to the same thing;
      - Promote hard-coded filepath to command arguments on the 'daemon' command

--- crates/amaru/src/bin/amaru/cmd/mod.rs
+++ crates/amaru/src/bin/amaru/cmd/mod.rs
@@ -3,6 +3,12 @@ use amaru::sync::Point;
 pub(crate) mod daemon;
 pub(crate) mod import;
 
+/// Default path to the on-disk ledger storage.
+pub(crate) const DEFAULT_LEDGER_DB_DIR: &str = "./ledger.db";
+
+/// Default path to pre-computed on-chain data needed for block header validation.
+pub(crate) const DEFAULT_DATA_DIR: &str = "./data";
+
 pub(crate) fn parse_point<'a, F, E>(raw_str: &str, bail: F) -> Result<Point, E>
 where
     F: Fn(&'a str) -> E + 'a,
