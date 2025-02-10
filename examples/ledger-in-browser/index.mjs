f8837e93ef5607b177c67c3c2a4924908e8959d9
jeluard <jeluard@users.noreply.github.com>
2025-02-10 15:58:39 +0100

    feat: added simple WASM example
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

---
+++ examples/ledger-in-browser/index.mjs
@@ -0,0 +1,8 @@
+import fs from 'node:fs/promises';
+
+const wasmBuffer = await fs.readFile('assets/amaru_ledger_in_browser.wasm');
+const wasm = await WebAssembly.instantiate(wasmBuffer);
+
+wasm.instance.exports.ledger();
+
+console.log("Done");
