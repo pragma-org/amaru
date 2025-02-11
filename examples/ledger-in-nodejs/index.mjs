9f12fff1e05f92b0a7bcec88fd36fea1f87f9e2c
jeluard <jeluard@users.noreply.github.com>
2025-02-11 14:43:17 +0100

    fix: wrong wasm path
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- examples/ledger-in-nodejs/index.mjs
+++ examples/ledger-in-nodejs/index.mjs
@@ -1,6 +1,6 @@
 import fs from 'node:fs/promises';
 
-const wasmBuffer = await fs.readFile('assets/amaru_ledger_in_nodejs.wasm');
+const wasmBuffer = await fs.readFile('assets/amaru_example_ledger_in_nodejs.wasm');
 const wasm = await WebAssembly.instantiate(wasmBuffer);
 
 wasm.instance.exports.ledger();
