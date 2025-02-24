d601e3917bd87f554a8f7f5fdd4239ef27fcd10e
jeluard <jeluard@users.noreply.github.com>
2025-02-24 15:18:00 +0100

    chore: fix examples
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- examples/ledger-in-nodejs/src/lib.rs
+++ examples/ledger-in-nodejs/src/lib.rs
@@ -1,6 +1,6 @@
 use amaru_examples_shared::{forward_ledger, RAW_BLOCK_CONWAY_1};
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 #[allow(clippy::missing_safety_doc)]
 pub unsafe extern "C" fn ledger() {
     forward_ledger(RAW_BLOCK_CONWAY_1);
