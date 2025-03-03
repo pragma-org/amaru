58cea7ab82ecc38cd51bca7163105f407ea4fc25
jeluard <jeluard@users.noreply.github.com>
2025-03-03 15:58:42 +0100

    chore: use tick for lifetime extension
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- crates/amaru-stores/src/rocksdb/columns/dreps.rs
+++ crates/amaru-stores/src/rocksdb/columns/dreps.rs
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::collections::BTreeSet;
+
 use crate::rocksdb::common::{as_key, as_value, PREFIX_LEN};
-use amaru_kernel::Epoch;
+use amaru_kernel::{Epoch, StakeCredential};
 use amaru_ledger::store::StoreError;
 use rocksdb::Transaction;
 use tracing::error;
@@ -27,9 +29,10 @@ pub const PREFIX: [u8; PREFIX_LEN] = [0x64, 0x72, 0x65, 0x70];
 #[allow(clippy::unwrap_used)]
 pub fn add<DB>(
     db: &Transaction<'_, DB>,
-    rows: impl Iterator<Item = (Key, Value, Option<Epoch>)>,
+    rows: impl Iterator<Item = (Key, Value)>,
+    epoch: Epoch,
 ) -> Result<(), StoreError> {
-    for (credential, (anchor, deposit), epoch) in rows {
+    for (credential, (anchor, deposit)) in rows {
         let key = as_key(&PREFIX, &credential);
 
         // In case where a registration already exists, then we must only update the underlying
@@ -43,16 +46,14 @@ pub fn add<DB>(
             if let Some(deposit) = deposit {
                 row.deposit = deposit;
             }
-            if let Some(epoch) = epoch {
-                row.last_interaction = epoch;
-            }
+            // Do not update the last interaction epoch as this is an existing DRep.
             db.put(key, as_value(row))
                 .map_err(|err| StoreError::Internal(err.into()))?;
         } else if let Some(deposit) = deposit {
             let row = Row {
                 anchor,
                 deposit,
-                last_interaction: epoch.unwrap(), // Cannot fail; this is a new registration.
+                last_interaction: epoch,
             };
             db.put(key, as_value(row))
                 .map_err(|err| StoreError::Internal(err.into()))?;
@@ -61,6 +62,36 @@ pub fn add<DB>(
                 target: EVENT_TARGET,
                 ?credential,
                 "add.register_no_deposit",
+            )
+        };
+    }
+
+    Ok(())
+}
+
+pub fn tick<DB>(
+    db: &Transaction<'_, DB>,
+    credentials: BTreeSet<StakeCredential>,
+    epoch: Epoch,
+) -> Result<(), StoreError> {
+    for credential in credentials {
+        let key = as_key(&PREFIX, &credential);
+
+        // In case where a registration already exists, then we must only update the underlying
+        // entry.
+        if let Some(mut row) = db
+            .get(&key)
+            .map_err(|err| StoreError::Internal(err.into()))?
+            .map(Row::unsafe_decode)
+        {
+            row.last_interaction = epoch;
+            db.put(key, as_value(row))
+                .map_err(|err| StoreError::Internal(err.into()))?;
+        } else {
+            error!(
+                target: EVENT_TARGET,
+                ?credential,
+                "tick.unknown_drep",
             )
         };
     }
