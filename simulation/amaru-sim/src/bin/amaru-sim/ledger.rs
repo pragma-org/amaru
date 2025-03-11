b954cb932947002a94e0429c681b5a3c0eb4c360
Arnaud Bailly <arnaud@pankzsoft.com>
2025-03-11 08:46:41 +0100

    Correctly compute total stake as sum of individual stake
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

--- simulation/amaru-sim/src/bin/amaru-sim/ledger.rs
+++ simulation/amaru-sim/src/bin/amaru-sim/ledger.rs
@@ -22,37 +22,23 @@ use pallas_crypto::hash::Hash;
 use serde::{Deserialize, Serialize};
 use serde_json::Error;
 
-// Fake stake distribution
-
-// {
-//   "chain": "dd6bb6e544c08294a95f2adcf8fe2766067e15eb8e31c1cdb0287f68e9346e0d",
-//   "coldSignKey": "9d90778c89b3287651d9a85770732fc031ebdebf3334d1e1da7519a75f2fd15f",
-//   "individualStake": {
-//     "individualPoolStake": {
-//       "denominator": 10,
-//       "numerator": 1
-//     },
-//     "individualPoolStakeVrf": "650c43fc40bd6430cc5902e6e0555d91a1fdf14122272b92ae8e27ccf9675009",
-//     "individualTotalPoolStake": 1000000000000000
-//   },
-//   "kesSignKey": "86c3972eb54d56a5fc4d892e0353cfa5882575e8c9b59ca7e46c2560a58a15e20ee09b2c885ef7ae4dec8b00a60c766b8fdf68858ba2af863d802b6000f86dd2a83cfed24ad8e97da87d6c75fa334522615d1dcedeb0caf754b5364e29864d81be13107f43adea6bc5e361b6904fb84316655f062b819f6b254418f78225b2bc553b10e7e788c9756e00de4043070353f034198e5c530721786c29e3e3db3b3fe0d7eff0156b448dcaec38a967e689404984e7b2e2330317b17e88566141550db26a5b7705b3240c5fc17c19bbce87ea90fb7b0c60bb182373ec484e7dd0809c6c9c1e2a4c532302845f6f3ac580facfa9e17d4c56617da0239d7d12e310a1ccb96daf53de9beca8dd6fa7ba6fcb31461b0b59d8b9a10691979d1b99a323721f941f60a444f28426920c2751323ba28b365c9a7db4471231043fa43b3ded1e43755615a87ae050662b0f59dfbd0575aa3774d619982f1fe34f798796b1d721943e685dd28d54f7d32dc695be8c2840a4f1d41e14d5cc21891747c35dc45c57204ba6608a0e792e90b3bc8699ca829bd07b469d276dea25e2aab185bc648d2aca1658ac77b073e2929170904650021dd9be5e0fdb9f953dec4687c4c53d772437718af1c87edbe43ad31d9febae2e518a320cf4700dfbcf6e2ac1634db5f7a5b337c0523b1cc7be656963be1bd94c8da1683a8bc737c3f8e0d45219b90a2faa9ba6244ee9aab869697da9c8cd398dc9cf3711315afde182194a0a0377fd8aa8e34fe26eaffa9bbe1a14592c3ebcf693a6dd65edeeaebf1a664536f98dfa7b9a3491170cf1b06e846af02edf754206372b3d77bf250bebbeec9bc22ada298b4e1b",
-//   "ocertCounter": 47,
-//   "poolId": "650c43fc40bd6430cc5902e6e0555d91a1fdf14122272b92ae8e27ccf9675009",
-//   "poolIdx": 1,
-//   "vrfSignKey": "97bc9670f28ad98b9cd3a2c02fdb504a831756163dd19dc0fc983ebe7f1df51f3ef06e6b84c658e419219cd960f20885fdb889e37b0323940ea5a2b904db9c97"
-// }
-
+/// Stake data for a single pool.
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct IndividualStake {
+pub struct IndividualStake {
+    /// The ratio of stake for a single pool.
     individual_pool_stake: RationalNumber,
+
+    /// The hash of the VRF key for the pool.
     individual_pool_stake_vrf: Hash<32>,
+
+    /// The total stake for the pool.
     individual_total_pool_stake: u64,
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct FakeStakePoolInfo {
+pub struct FakeStakePoolInfo {
     cold_sign_key: String,
     individual_stake: IndividualStake,
     kes_sign_key: String,
@@ -62,24 +48,29 @@ struct FakeStakePoolInfo {
     vrf_sign_key: String,
 }
 
-impl FakeStakePoolInfo {
-    fn stake(&self) -> u64 {
-        let RationalNumber {
-            denominator,
-            numerator,
-        } = self.individual_stake.individual_pool_stake;
-        numerator * self.individual_stake.individual_total_pool_stake / denominator
-    }
-}
-
 pub struct FakeStakeDistribution {
+    total_active_stake: u64,
     pools: Vec<FakeStakePoolInfo>,
 }
 
 impl FakeStakeDistribution {
     pub fn from_file(stake_distribution_file: &Path) -> Result<FakeStakeDistribution, Error> {
         let file = File::open(stake_distribution_file).unwrap();
-        serde_json::from_reader(BufReader::new(file)).map(|pools| FakeStakeDistribution { pools })
+
+        serde_json::from_reader(BufReader::new(file))
+            .map(|pools: Vec<FakeStakePoolInfo>| FakeStakeDistribution::new(pools))
+    }
+
+    pub fn new(pools: Vec<FakeStakePoolInfo>) -> FakeStakeDistribution {
+        let total_active_stake = pools
+            .iter()
+            .map(|p| p.individual_stake.individual_total_pool_stake)
+            .sum();
+
+        FakeStakeDistribution {
+            pools,
+            total_active_stake,
+        }
     }
 }
 
@@ -94,8 +85,8 @@ impl HasStakeDistribution for FakeStakeDistribution {
             .find(|p| p.pool_id == *pool)
             .map(|info| PoolSummary {
                 vrf: info.individual_stake.individual_pool_stake_vrf,
-                stake: info.stake(),
-                active_stake: info.individual_stake.individual_total_pool_stake,
+                stake: info.individual_stake.individual_total_pool_stake,
+                active_stake: self.total_active_stake,
             })
     }
 
@@ -189,5 +180,24 @@ mod test {
         );
 
         assert!(stake_distribution.get_pool(42, &pool_id).is_some())
+    }
+
+    #[test]
+    fn compute_total_stake_from_individual_pool_stake() {
+        let stake_distribution_file = "tests/data/stake-distribution.json";
+        let stake_distribution =
+            FakeStakeDistribution::from_file(&PathBuf::from(stake_distribution_file)).unwrap();
+        let pool_id: Hash<28> = amaru_kernel::Hash::from(
+            hex::decode("50484a702b93327308d85f51f1831940fdddcb751bf43bc3376c42b9")
+                .unwrap()
+                .as_slice(),
+        );
+
+        assert_eq!(
+            Some(1250000000000000),
+            stake_distribution
+                .get_pool(42, &pool_id)
+                .map(|p| p.active_stake)
+        )
     }
 }
