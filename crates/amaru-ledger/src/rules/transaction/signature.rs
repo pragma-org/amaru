98f1b9e6c403d87b02fefa34af1a16640fffa579
yHSJ <josh@securitybot.info>
2025-03-14 21:01:43 -0400

    wip: signature validation
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-ledger/src/rules/transaction/signature.rs
+++ crates/amaru-ledger/src/rules/transaction/signature.rs
@@ -1,5 +1,8 @@
+use std::ops::Deref;
+
 use amaru_kernel::{
-    get_payment_key_hash, HasAddress, Hasher, MintedTransactionBody, NonEmptySet, VKeyWitness,
+    get_payment_key_hash, HasAddress, Hasher, MintedTransactionBody, NonEmptySet, PublicKey,
+    Signature, VKeyWitness,
 };
 
 use crate::rules::{context::UtxoSlice, TransactionRuleViolation};
@@ -37,9 +40,8 @@ pub fn validate_sigantures(
     .concat();
 
     let empty_vec = vec![];
+    let vkey_witnesses = vkey_witnesses.as_deref().unwrap_or(&empty_vec);
     let vkey_hashes = vkey_witnesses
-        .as_deref()
-        .unwrap_or(&empty_vec)
         .iter()
         .map(|witness| Hasher::<224>::hash(&witness.vkey))
         .collect::<Vec<_>>();
@@ -52,6 +54,32 @@ pub fn validate_sigantures(
 
     if !missing_key_hashes.is_empty() {
         return Err(TransactionRuleViolation::MissingRequiredWitnesses { missing_key_hashes });
+    }
+
+    for witness in vkey_witnesses {
+        // TODO: rust-ier way to handle this? I really don't to clone every key and signature for every single witness in every tx...
+        let vkey_bytes: [u8; 32] =
+            witness
+                .vkey
+                .deref()
+                .clone()
+                .try_into()
+                .unwrap_or_else(|v: Vec<_>| {
+                    panic!("Invalid vkey length {} (expected 32 bytes)", v.len())
+                });
+        let signature_bytes: [u8; 64] = witness
+            .signature
+            .deref()
+            .clone()
+            .try_into()
+            .unwrap_or_else(|v: Vec<_>| {
+                panic!("Invalid signature length {} (expected 64 bytes)", v.len())
+            });
+
+        let public_key: PublicKey = vkey_bytes.into();
+        let signature: Signature = signature_bytes.into();
+        // TODO: validate signature
+        // let is_valid = public_key.verify(transaction_body, &signature);
     }
 
     todo!()
