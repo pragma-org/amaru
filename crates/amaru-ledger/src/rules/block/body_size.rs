7ce9b8ae19935649917e85ac9f3237fa6ba05369
KtorZ <matthias.benkort@gmail.com>
2025-03-23 08:18:05 +0100

    chore: add license preamble to various files that are missing it.
    Signed-off-by: KtorZ <matthias.benkort@gmail.com>

--- crates/amaru-ledger/src/rules/block/body_size.rs
+++ crates/amaru-ledger/src/rules/block/body_size.rs
@@ -1,6 +1,19 @@
-use amaru_kernel::{to_cbor, HeaderBody, MintedBlock};
+// Copyright 2025 PRAGMA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use crate::rules::RuleViolation;
+use amaru_kernel::{to_cbor, HeaderBody, MintedBlock};
 
 /// This validation checks that the purported block body size matches the actual block body size.
 /// The validation of the bounds happens in the networking layer
--- crates/amaru-ledger/src/rules/block/ex_units.rs
+++ crates/amaru-ledger/src/rules/block/ex_units.rs
@@ -1,6 +1,19 @@
-use amaru_kernel::{protocol_parameters::ProtocolParameters, sum_ex_units, ExUnits};
+// Copyright 2025 PRAGMA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 use crate::rules::RuleViolation;
+use amaru_kernel::{protocol_parameters::ProtocolParameters, sum_ex_units, ExUnits};
 
 pub fn block_ex_units_valid(
     ex_units: Vec<ExUnits>,
