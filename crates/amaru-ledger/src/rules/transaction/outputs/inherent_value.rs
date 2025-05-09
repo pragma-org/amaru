0f6bafe606b8d407f32ee8be0c1941ff8f198860
yHSJ <josh@securitybot.info>
2025-05-09 13:17:55 -0400

    feat: validate network and max value size in an output
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-ledger/src/rules/transaction/outputs/inherent_value.rs
+++ crates/amaru-ledger/src/rules/transaction/outputs/inherent_value.rs
@@ -22,17 +22,31 @@ pub fn execute(
     output: &MintedTransactionOutput<'_>,
 ) -> Result<(), InvalidOutput> {
     let coins_per_utxo_byte = protocol_parameters.coins_per_utxo_byte;
-
     // FIXME: do not re-serialize the output here, but rely on original bytes.
     let minimum_value = to_cbor(output).len() as u64 * coins_per_utxo_byte;
+
     let given_value = output.lovelace();
 
     if given_value < minimum_value {
-        Err(InvalidOutput::TooSmall {
+        return Err(InvalidOutput::TooSmall {
             minimum_value,
             given_value,
-        })
-    } else {
-        Ok(())
+        });
     }
+
+    let max_val_size = protocol_parameters.max_val_size;
+    // FIXME: do not re-serialize the value here, but rely on original bytes.
+    let given_val_size = match output {
+        amaru_kernel::PseudoTransactionOutput::Legacy(output) => to_cbor(&output.amount).len(),
+        amaru_kernel::PseudoTransactionOutput::PostAlonzo(output) => to_cbor(&output.value).len(),
+    };
+
+    if given_val_size > max_val_size {
+        return Err(InvalidOutput::ValueTooLarge {
+            maximum_size: max_val_size,
+            given_size: given_val_size,
+        });
+    }
+
+    Ok(())
 }
