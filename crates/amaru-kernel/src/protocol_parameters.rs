d527e2897caf3a275d60f64ffa22e58094685dd3
jeluard <jeluard@users.noreply.github.com>
2025-05-13 16:03:18 +0200

    chore: improve u64 decoding
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- crates/amaru-kernel/src/protocol_parameters.rs
+++ crates/amaru-kernel/src/protocol_parameters.rs
@@ -55,10 +55,8 @@ fn decode_u64(d: &mut Decoder<'_>) -> Result<u64, cbor::decode::Error> {
         cbor::data::Type::U8 => Ok(d.u8()? as u64),
         cbor::data::Type::U16 => Ok(d.u16()? as u64),
         cbor::data::Type::U32 => Ok(d.u32()? as u64),
-        cbor::data::Type::U64 => Ok(d.u64()? as u64),
-        _ => Err(cbor::decode::Error::message(
-            "Expected u8/u16/u32/u64 type",
-        )),
+        cbor::data::Type::U64 => Ok(d.u64()?),
+        _ => Err(cbor::decode::Error::message("Expected u8/u16/u32/u64 type")),
     }
 }
 
