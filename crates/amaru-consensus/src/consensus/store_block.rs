bf560143c7c3e828374ea4f23579de25349cb956
Arnaud Bailly <arnaud@pankzsoft.com>
2025-05-19 17:39:52 +0200

    feat: start adding unit tests for store_block
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

---
+++ crates/amaru-consensus/src/consensus/store_block.rs
@@ -0,0 +1,167 @@
+// Copyright 2025 PRAGMA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::{consensus::store::ChainStore, ConsensusError};
+use amaru_kernel::{block::BlockValidationResult, Header, Point, RawBlock};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+pub struct StoreBlock {
+    store: Arc<Mutex<dyn ChainStore<Header>>>,
+}
+
+impl StoreBlock {
+    pub fn new(chain_store: Arc<Mutex<dyn ChainStore<Header>>>) -> Self {
+        StoreBlock { store: chain_store }
+    }
+
+    pub async fn store(&self, point: &Point, block: &RawBlock) -> Result<(), ConsensusError> {
+        self.store
+            .lock()
+            .await
+            .store_block(&point.into(), block)
+            .map_err(|e| ConsensusError::StoreBlockFailed(point.clone(), e))
+    }
+
+    pub async fn handle_event(
+        &self,
+        event: BlockValidationResult,
+    ) -> Result<BlockValidationResult, ConsensusError> {
+        match event {
+            BlockValidationResult::BlockValidated { point, block, span } => {
+                self.store(&point, &block).await?;
+                Ok(BlockValidationResult::BlockValidated { point, block, span })
+            },
+            BlockValidationResult::BlockValidationFailed { point, span } => {
+                // No action needed for failed validation, just pass through the event
+                Ok(BlockValidationResult::BlockValidationFailed { point, span })
+            },
+            BlockValidationResult::RolledBackTo {
+                rollback_point,
+                span,
+            } => todo!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::consensus::store::StoreError;
+
+    use super::*;
+    use amaru_kernel::{Hash, Point, RawBlock};
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
+    use tracing::Span;
+
+    // Mock implementation of ChainStore for testing
+    struct MockChainStore {
+        stored_blocks: std::collections::HashMap<Hash<32>, RawBlock>,
+    }
+
+    impl MockChainStore {
+        fn new() -> Self {
+            Self {
+                stored_blocks: std::collections::HashMap::new(),
+            }
+        }
+    }
+
+    impl ChainStore<Header> for MockChainStore {
+        fn store_block(&mut self, point: &Hash<32>, block: &RawBlock) -> Result<(), StoreError> {
+            self.stored_blocks.insert(*point, block.clone());
+            Ok(())
+        }
+
+        fn load_header(&self, hash: &Hash<32>) -> Option<Header> {
+            todo!()
+        }
+
+        fn store_header(&mut self, hash: &Hash<32>, header: &Header) -> Result<(), StoreError> {
+            todo!()
+        }
+
+        fn load_block(&self, hash: &Hash<32>) -> Option<RawBlock> {
+            todo!()
+        }
+
+        fn get_nonces(&self, header: &Hash<32>) -> Option<amaru_ouroboros::Nonces> {
+            todo!()
+        }
+
+        fn put_nonces(&mut self, header: &Hash<32>, nonces: &amaru_ouroboros::Nonces) -> Result<(), StoreError> {
+            todo!()
+        }
+
+        fn era_history(&self) -> &amaru_kernel::EraHistory {
+            todo!()
+        }
+    }
+
+    #[tokio::test]
+    async fn test_handle_event_block_validated() {
+        // Setup
+        let mock_store = Arc::new(Mutex::new(MockChainStore::new()));
+        let store_block = StoreBlock::new(mock_store.clone());
+
+        // Create test data
+        let point = Point::Specific(123, Hash::from([1; 32]).to_vec());
+        let block = vec![0, 1, 2, 3];
+        let span = Span::current();
+
+        // Create a BlockValidated event
+        let event = BlockValidationResult::BlockValidated {
+            point: point.clone(),
+            block: block.clone(),
+            span,
+        };
+
+        // Call handle_event
+        let result = store_block.handle_event(event).await;
+
+        // Since the implementation is todo!(), this will panic
+        // Once implemented, we would expect:
+        assert!(result.is_ok());
+
+   }
+
+    #[tokio::test]
+    async fn test_handle_event_block_validation_failed() {
+        // Setup
+        let mock_store = Arc::new(Mutex::new(MockChainStore::new()));
+        let store_block = StoreBlock::new(mock_store.clone());
+
+        // Create test data
+        let point = Point::Specific(123, Hash::from([1; 32]).to_vec());
+        let span = Span::current();
+
+        // Create a BlockValidationFailed event
+        let event = BlockValidationResult::BlockValidationFailed {
+            point: point.clone(),
+            span,
+        };
+
+        // Call handle_event
+        let result = store_block.handle_event(event).await;
+
+        assert!(result.is_ok());
+        let result_event = result.unwrap();
+        match result_event {
+            BlockValidationResult::BlockValidationFailed { point: result_point, span: _ } => {
+                assert_eq!(result_point, point);
+            },
+            _ => panic!("Expected BlockValidationFailed event"),
+        }
+   }
+}
