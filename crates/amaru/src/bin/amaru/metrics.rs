db9f087be9f2f9af6043bfb11aca2a7dc4b3e33d
KtorZ <matthias.benkort@gmail.com>
2025-07-10 11:25:53 +0200

    chore: refactor metrics code in preparation of upcoming change.
    Signed-off-by: KtorZ <matthias.benkort@gmail.com>

--- crates/amaru/src/bin/amaru/metrics.rs
+++ crates/amaru/src/bin/amaru/metrics.rs
@@ -13,27 +13,28 @@
 // limitations under the License.
 
 use opentelemetry_sdk::metrics::SdkMeterProvider;
+use std::{sync::LazyLock, time::Duration};
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
 use tokio::task::JoinHandle;
+use tracing::error;
 
-pub fn track_system_metrics(metrics: SdkMeterProvider) -> JoinHandle<()> {
+static METRICS_POLL_DELAY: LazyLock<Duration> = LazyLock::new(|| Duration::from_secs(1));
+
+pub fn track_system_metrics(provider: SdkMeterProvider) -> JoinHandle<()> {
     use internals::*;
-    use std::time::Duration;
-
     tokio::spawn(async move {
-        let counters = make_system_counters(metrics);
-        // TODO(pi): configurable parameter?
-        let delay = Duration::from_secs(1);
+        let metrics = ProcessMetrics::new(provider);
         let mut sys = System::new_with_specifics(
             RefreshKind::nothing()
                 .with_cpu(CpuRefreshKind::everything().without_frequency())
                 .with_memory(MemoryRefreshKind::everything().without_swap()),
         );
         loop {
-            tokio::time::sleep(delay).await;
-
+            tokio::time::sleep(*METRICS_POLL_DELAY).await;
             sys.refresh_cpu_all();
-            record_system_metrics(&sys, &counters);
+            metrics
+                .record(&sys)
+                .unwrap_or_else(|err| error!("failed polling metrics: {err}"))
         }
     })
 }
@@ -46,52 +47,59 @@ mod internals {
     use opentelemetry_sdk::metrics::SdkMeterProvider;
     use sysinfo::System;
 
-    pub struct SystemCounters {
+    pub struct ProcessMetrics {
         total_memory: Gauge<u64>,
         free_memory: Gauge<u64>,
         cpu_usage: Gauge<f64>,
     }
 
-    pub fn make_system_counters(metrics: SdkMeterProvider) -> SystemCounters {
-        // TODO: standardize with the Haskell node somehow?
-        let meter = metrics.meter("system");
-        let total_memory = meter
-            .u64_gauge("memory.limit")
-            .with_description("The total system memory, updated once per second")
-            .with_unit("MB")
-            .build();
+    impl ProcessMetrics {
+        pub fn new(metrics: SdkMeterProvider) -> Self {
+            // TODO: standardize with the Haskell node somehow?
+            let meter = metrics.meter("amaru");
 
-        let free_memory = meter
-            .u64_gauge("memory.usage")
-            .with_description("The free system memory, measured once per second")
-            .with_unit("MB")
-            .build();
+            let total_memory = meter
+                .u64_gauge("memory.limit")
+                .with_description("The total system memory, updated once per second")
+                .with_unit("MB")
+                .build();
 
-        let cpu_usage = meter
-            .f64_gauge("cpu.percent")
-            .with_description("the cpu usage in percent, measured once per second")
-            .with_unit("%")
-            .build();
+            let free_memory = meter
+                .u64_gauge("memory.usage")
+                .with_description("The free system memory, measured once per second")
+                .with_unit("MB")
+                .build();
 
-        SystemCounters {
-            total_memory,
-            free_memory,
-            cpu_usage,
+            let cpu_usage = meter
+                .f64_gauge("cpu.percent")
+                .with_description("the cpu usage in percent, measured once per second")
+                .with_unit("%")
+                .build();
+
+            Self {
+                total_memory,
+                free_memory,
+                cpu_usage,
+            }
         }
-    }
 
-    pub fn record_system_metrics(sys: &System, counters: &SystemCounters) {
-        counters.total_memory.record(sys.total_memory(), &[]);
-        counters.free_memory.record(sys.free_memory(), &[]);
-        let usages = sys
-            .cpus()
-            .iter()
-            .map(|cpu| (cpu.name().into(), cpu.cpu_usage()))
-            .collect::<Vec<(String, f32)>>();
-        for cpu in usages {
-            counters
-                .cpu_usage
-                .record(cpu.1 as f64, &[KeyValue::new("cpu_name", cpu.0)]);
+        pub fn record(&self, sys: &System) -> Result<(), Box<dyn std::error::Error>> {
+            self.total_memory.record(sys.total_memory(), &[]);
+
+            self.free_memory.record(sys.free_memory(), &[]);
+
+            let usages = sys
+                .cpus()
+                .iter()
+                .map(|cpu| (cpu.name().into(), cpu.cpu_usage()))
+                .collect::<Vec<(String, f32)>>();
+
+            for cpu in usages {
+                self.cpu_usage
+                    .record(cpu.1 as f64, &[KeyValue::new("cpu_name", cpu.0)]);
+            }
+
+            Ok(())
         }
     }
 }
