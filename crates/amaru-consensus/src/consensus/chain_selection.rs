e8815893af0cce3f20be389a0422f5c189518b67
Arnaud Bailly <arnaud@pankzsoft.com>
2025-08-06 16:45:42 +0200

    refactor: extract tip to own module
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

--- crates/amaru-consensus/src/consensus/chain_selection.rs
+++ crates/amaru-consensus/src/consensus/chain_selection.rs
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use super::tip::Tip;
 use crate::{peer::Peer, ConsensusError};
-use amaru_kernel::{cbor, Point, HEADER_HASH_SIZE};
+use amaru_kernel::Point;
 use amaru_ouroboros_traits::is_header::IsHeader;
 use pallas_crypto::hash::Hash;
 use std::{collections::BTreeMap, fmt::Debug};
@@ -102,90 +103,6 @@ impl<H: IsHeader + Clone> Fragment<H> {
 
         // Remove the oldest headers
         self.headers = self.headers.split_off(to_remove);
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Tip<H: IsHeader> {
-    Genesis,
-    Hdr(H),
-}
-
-impl<H, C> cbor::encode::Encode<C> for Tip<H>
-where
-    H: cbor::encode::Encode<C> + IsHeader,
-{
-    fn encode<W: cbor::encode::Write>(
-        &self,
-        e: &mut cbor::Encoder<W>,
-        ctx: &mut C,
-    ) -> Result<(), cbor::encode::Error<W::Error>> {
-        match self {
-            Tip::Genesis => e.encode(0).map(|_| ()),
-            Tip::Hdr(header) => header.encode(e, ctx),
-        }
-    }
-}
-
-impl<H: IsHeader> Tip<H> {
-    fn is_parent_of(&self, header: &Tip<H>) -> bool {
-        match (header.parent(), self) {
-            (None, Tip::Genesis) => true,
-            (Some(p), Tip::Hdr(hdr)) => p == hdr.hash(),
-            _ => false,
-        }
-    }
-}
-impl<H: IsHeader> IsHeader for Tip<H> {
-    fn hash(&self) -> Hash<HEADER_HASH_SIZE> {
-        match self {
-            Tip::Genesis => Hash::from([0; HEADER_HASH_SIZE]),
-            Tip::Hdr(header) => header.hash(),
-        }
-    }
-
-    fn point(&self) -> Point {
-        match self {
-            Tip::Genesis => Point::Origin,
-            Tip::Hdr(header) => header.point(),
-        }
-    }
-
-    fn parent(&self) -> Option<Hash<HEADER_HASH_SIZE>> {
-        match self {
-            Tip::Genesis => None,
-            Tip::Hdr(header) => header.parent(),
-        }
-    }
-
-    fn block_height(&self) -> u64 {
-        match self {
-            Tip::Genesis => 0,
-            Tip::Hdr(header) => header.block_height(),
-        }
-    }
-
-    fn slot(&self) -> u64 {
-        match self {
-            Tip::Genesis => 0,
-            Tip::Hdr(header) => header.slot(),
-        }
-    }
-
-    fn extended_vrf_nonce_output(&self) -> Vec<u8> {
-        match self {
-            Tip::Genesis => vec![],
-            Tip::Hdr(header) => header.extended_vrf_nonce_output(),
-        }
-    }
-}
-
-impl<H: IsHeader> From<Option<H>> for Tip<H> {
-    fn from(tip: Option<H>) -> Tip<H> {
-        match tip {
-            Some(header) => Tip::Hdr(header),
-            None => Tip::Genesis,
-        }
     }
 }
 
