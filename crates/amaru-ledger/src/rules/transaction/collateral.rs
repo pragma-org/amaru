5a294a506e2d591ba68cd11b9f2f01ab534d1ee9
yHSJ <josh@securitybot.info>
2025-09-11 13:26:44 -0400

    chore: add license header
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-ledger/src/rules/transaction/collateral.rs
+++ crates/amaru-ledger/src/rules/transaction/collateral.rs
@@ -1,3 +1,17 @@
+// Copyright 2025 PRAGMA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::collections::BTreeMap;
 
 use amaru_kernel::{
@@ -198,13 +212,13 @@ where
         });
     }
 
-    if let Some(expected_balance) = tx_collateral {
-        if expected_balance != balance.coin as u64 {
-            return Err(InvalidCollateral::IncorrectTotalCollateral {
-                provided: balance.coin as u64,
-                expected: expected_balance,
-            });
-        }
+    if let Some(expected_balance) = tx_collateral
+        && expected_balance != balance.coin as u64
+    {
+        return Err(InvalidCollateral::IncorrectTotalCollateral {
+            provided: balance.coin as u64,
+            expected: expected_balance,
+        });
     }
 
     Ok(())
