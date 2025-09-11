97f72f46e371f6ddc4b4183cb16fa7900523118d
yHSJ <josh@securitybot.info>
2025-09-11 13:26:44 -0400

    chore: add license header
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-kernel/src/memoized/transaction_output.rs
+++ crates/amaru-kernel/src/memoized/transaction_output.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use pallas_primitives::{conway::Multiasset, Bytes, Hash, PolicyId, PositiveCoin};
+use pallas_primitives::{Bytes, Hash, PolicyId, PositiveCoin, conway::Multiasset};
 
 use crate::{
     Address, AlonzoValue, AssetName, KeyValuePairs, Lovelace, MemoizedDatum, MemoizedNativeScript,
