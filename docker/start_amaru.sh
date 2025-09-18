0cef03b4eac136316616276805bf5599c07b5abd
Arnaud Bailly <arnaud@pankzsoft.com>
2025-09-18 22:14:16 +0200

    fix: trim down amaru logs
    Ended up being 142GB of logs in AT run 😂
    
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

--- docker/start_amaru.sh
+++ docker/start_amaru.sh
@@ -26,10 +26,10 @@ fi
 
 # keep stack traces for troubleshooting purposes
 export RUST_BACKTRACE=full
-export AMARU_LOG=debug
-export AMARU_TRACE=gasket=error,debug
 
-exec cargo run --profile dev -- --with-json-traces daemon \
+export AMARU_LOG=gasket=error,amaru=debug,amaru::stages::consensus::forward_chain=info,info
+
+exec cargo run --profile dev -- daemon \
       --peer-address "${AMARU_PEER_ADDRESS}" \
       --ledger-dir "${AMARU_LEDGER_DIR}" \
       --chain-dir "${AMARU_CHAIN_DIR}"
