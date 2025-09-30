ca5654cdcb0fe363d8ec50ac8959bae107ad2e94
Pascal Grange <pascal@grange.nom.fr>
2025-09-30 10:32:13 +0200

    chore: test cli explains there's a problem with fd
    Signed-off-by: Pascal Grange <pascal@grange.nom.fr>

--- cli-tests/test_crash_with_helpful_information.sh
+++ cli-tests/test_crash_with_helpful_information.sh
@@ -4,12 +4,19 @@ test_explains_snapshot_file_is_missing() {
 	assert_matches "data/preprod/snapshots.json.*NotFound" "$(bootstrap_amaru)"
 }
 
+skip_if "! ulimit -n" fd_limit
+test_explains_fd_limit_is_too_low() {
+	ulimit -n 256
+	assert_matches "Increase the limit for open files before starting Amaru" "$(start_amaru)"
+}
+
 setup() {
 	export AMARU_NETWORK=preprod
 	export CONFIG_FOLDER=data
 	export LEDGER_DIR=./ledger.${AMARU_NETWORK}.db
 	export CHAIN_DIR=./chain.${AMARU_NETWORK}.db
 	export BUILD_PROFILE=dev
+	export UNREACHABLE_PEER=127.0.0.1:65532
 }
 
 given_snapshots_file_is_missing() {
@@ -19,6 +26,14 @@ given_snapshots_file_is_missing() {
 bootstrap_amaru() {
 	cargo run --profile ${BUILD_PROFILE} -- bootstrap \
 		--config-dir ${CONFIG_FOLDER} \
+		--ledger-dir ${LEDGER_DIR} \
+		--chain-dir ${CHAIN_DIR} \
+		2>&1
+}
+
+start_amaru() {
+	cargo run --profile ${BUILD_PROFILE} -- daemon \
+		--peer-address ${UNREACHABLE_PEER} \
 		--ledger-dir ${LEDGER_DIR} \
 		--chain-dir ${CHAIN_DIR} \
 		2>&1
