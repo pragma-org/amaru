21b6bf097c39c99bd136c108510e6af6c8a00ccd
Arnaud Bailly <arnaud@pankzsoft.com>
2025-10-15 17:33:34 +0200

    feat: scaffold 'migrate_chain_db' command
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

---
+++ crates/amaru/src/bin/amaru/cmd/migrate_chain_db.rs
@@ -0,0 +1,34 @@
+// Copyright 2025 PRAGMA
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use amaru_stores::rocksdb::RocksDbConfig;
+use amaru_stores::rocksdb::consensus::{check_db_version, open_db};
+use clap::{Parser, arg};
+use std::{error::Error, path::PathBuf};
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    /// The path to the chain database to migrate
+    #[arg(long, value_name = "DIR", default_value = "chain.db")]
+    chain_dir: PathBuf,
+}
+
+pub async fn run(args: Args) -> Result<(), Box<dyn Error>> {
+    let chain_dir = args.chain_dir;
+    let mut config = RocksDbConfig::new(chain_dir);
+    config.create_if_missing = false;
+
+    let (_, db) = open_db(&config)?;
+    check_db_version(&db).or_else(|_| Ok(()))
+}
