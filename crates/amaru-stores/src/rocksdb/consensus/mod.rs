ea4231d30d524a853f20b2002639f0ad4a8f8889
Arnaud Bailly <arnaud@pankzsoft.com>
2025-10-24 23:09:27 +0200

    fix: do not compare best_chain_hash and store_chain.roll_forward
    Those two values are set at different places in the pipeline and they
    might not be necessary in sync: if we ran select_chain several time
    before running store_chain, the best_chain_hash will be over the
    expected value.
    
    This clearly points to the fact we should only update best chain in
    one place, eg. select_chain should not update the DB.
    
    Signed-off-by: Arnaud Bailly <arnaud@pankzsoft.com>

--- crates/amaru-stores/src/rocksdb/consensus/mod.rs
+++ crates/amaru-stores/src/rocksdb/consensus/mod.rs
@@ -18,8 +18,8 @@ pub mod util;
 pub use migration::*;
 
 use amaru_kernel::{
-    BlockHeader, HEADER_HASH_SIZE, Hash, HeaderHash, IsHeader, ORIGIN_HASH, Point, RawBlock, Slot,
-    cbor, from_cbor, to_cbor,
+    BlockHeader, HEADER_HASH_SIZE, Hash, HeaderHash, IsHeader, ORIGIN_HASH, Point, RawBlock, cbor,
+    from_cbor, to_cbor,
 };
 use amaru_ouroboros_traits::{
     ChainStore, DiagnosticChainStore, Nonces, ReadOnlyChainStore, StoreError,
@@ -399,23 +399,6 @@ impl<H: IsHeader + Clone + Debug + for<'d> cbor::Decode<'d, ()>> ChainStore<H> f
     }
 
     fn roll_forward_chain(&self, point: &Point) -> Result<(), StoreError> {
-        let tip_hash = <Self as ReadOnlyChainStore<BlockHeader>>::get_best_chain_hash(self);
-
-        if let Some(header) =
-            <Self as ReadOnlyChainStore<BlockHeader>>::load_header(self, &tip_hash)
-        {
-            let slot = Slot::from(header.slot());
-            if slot >= point.slot_or_default() {
-                return Err(StoreError::ReadError {
-                    error: format!(
-                        "Cannot roll forward chain to point {:?} as current tip {:?} is at equal or higher slot",
-                        point,
-                        header.slot(),
-                    ),
-                });
-            }
-        }
-
         store_chain_point(&self.db, point)
     }
 
