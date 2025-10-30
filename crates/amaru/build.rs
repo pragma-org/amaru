2784d96c876a373339c4a2a2d95117e112af4c12
yHSJ <josh@securitybot.info>
2025-10-30 15:43:07 -0500

    chore: use build script to generate tests
    Signed-off-by: yHSJ <josh@securitybot.info>
    Signed-off-by: ruko <rukokarasu@gmail.com>

--- crates/amaru/build.rs
+++ crates/amaru/build.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::env;
+use std::{env, fs, path::Path};
 
 #[expect(clippy::expect_used)]
 fn main() {
@@ -23,4 +23,53 @@ fn main() {
     let network = env::var("AMARU_NETWORK").unwrap_or_else(|_| "preprod".into());
     println!("cargo:rerun-if-env-changed=AMARU_NETWORK");
     println!("cargo:rustc-env=AMARU_NETWORK={}", network);
+
+    get_conformance_test_vectors();
+}
+
+#[allow(clippy::unwrap_used)]
+fn get_conformance_test_vectors() {
+    let test_dir = "../../cardano-blueprint/src/ledger/conformance-test-vectors/eras/conway";
+    println!("cargo:rerun-if-changed={}", test_dir);
+
+    let mut files = Vec::new();
+    visit_dirs(Path::new(test_dir), &mut files);
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("test_cases.rs");
+
+    let mut output = String::new();
+    for path in files {
+        if path.contains("pparams-by-hash") {
+            continue;
+        }
+
+        output.push_str(&format!("#[test_case(\"{}\")]\n", path));
+    }
+
+    output.push_str(
+        r#"
+#[allow(clippy::unwrap_used)]
+pub fn compare_snapshot_test_case(snapshot: &str) {
+    evaluate_vector(snapshot).unwrap()
+}
+"#,
+    );
+
+    fs::write(dest_path, output).unwrap();
+}
+
+fn visit_dirs(dir: &Path, files: &mut Vec<String>) {
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.flatten() {
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, files);
+            } else if path.is_file()
+                && let Some(path_str) = path.to_str()
+            {
+                files.push(path_str.to_string());
+            }
+        }
+    }
 }
--- crates/amaru/tests/evaluate_ledger_states.rs
+++ crates/amaru/tests/evaluate_ledger_states.rs
@@ -13,41 +13,36 @@
 // limitations under the License.
 
 use amaru_kernel::{
-    AnyCbor, AuxiliaryData, Bytes, Epoch, EraHistory, Hasher, KeepRaw, MintedTransactionBody, MintedTx,
-    MintedWitnessSet, Point, PoolId, TransactionInput, TransactionOutput, TransactionPointer, cbor, network::NetworkName, Network, Value,
+    AnyCbor, AuxiliaryData, Bytes, Epoch, EraHistory, Hasher, KeepRaw, MintedTransactionBody,
+    MintedTx, MintedWitnessSet, Network, TransactionPointer, cbor, network::NetworkName,
     protocol_parameters::ProtocolParameters,
 };
 use amaru_ledger::{
-    self,
-    context::DefaultValidationContext,
-    rules::transaction,
-    state::{self, volatile_db::StoreUpdate},
-    store::{EpochTransitionProgress, GovernanceActivity, ReadStore, Store, TransactionalContext},
+    self, context::DefaultValidationContext, rules::transaction, store::GovernanceActivity,
 };
-use amaru_stores::rocksdb::RocksDB;
-use std::{collections::{BTreeMap, BTreeSet}, env, fs, iter, ops::Deref, path::PathBuf};
-
-use test_case::test_case;
+use std::{collections::BTreeMap, env, fs, ops::Deref, path::PathBuf};
 
 use once_cell::sync::Lazy;
 use std::sync::Mutex;
 
 struct TestContext {
-    ledger_dir: PathBuf,
     pparams_dir: PathBuf,
 }
 
 fn get_test_context() -> Result<TestContext, Box<dyn std::error::Error>> {
     let working_dir = env::current_dir()?;
     Ok(TestContext {
-        ledger_dir: working_dir.join(PathBuf::from("../../ledger.db")),
         pparams_dir: working_dir.join(PathBuf::from("../../cardano-blueprint/src/ledger/conformance-test-vectors/eras/conway/impl/dump/pparams-by-hash/")),
     })
 }
 
 static TEST_MUTEX: Lazy<Mutex<()>> = Lazy::new(Mutex::default);
 
-include!("generated_ledger_conformance_test_cases.incl");
+#[allow(unused_imports)]
+use test_case::test_case;
+
+// Tests cases are constructed in build.rs, which generates the test_cases.rs file
+include!(concat!(env!("OUT_DIR"), "/test_cases.rs"));
 
 fn evaluate_vector(snapshot: &str) -> Result<(), Box<dyn std::error::Error>> {
     let _shared = TEST_MUTEX.lock()?;
@@ -57,7 +52,7 @@ fn evaluate_vector(snapshot: &str) -> Result<(), Box<dyn std::error::Error>> {
         Ok(tc) => {
             let vector_file = fs::read(snapshot)?;
             let record: TestVector = minicbor::decode(&vector_file)?;
-            let () = import_vector(record, &tc.ledger_dir, era_history, &tc.pparams_dir)?;
+            let () = import_vector(record, era_history, &tc.pparams_dir)?;
             Ok(())
         }
         Err(e) => {
@@ -68,12 +63,6 @@ fn evaluate_vector(snapshot: &str) -> Result<(), Box<dyn std::error::Error>> {
             Ok(())
         }
     }
-}
-
-fn peek_epoch(nes: &AnyCbor) -> Result<u64, Box<dyn std::error::Error>> {
-    let mut d = cbor::decode::Decoder::new(nes.raw_bytes());
-    let _ = d.array()?;
-    Ok(d.u64()?)
 }
 
 #[derive(cbor::Decode)]
@@ -103,7 +92,11 @@ impl<'b, C> minicbor::decode::Decode<'b, C> for TestVectorEvent {
         let variant = d.u16()?;
 
         match variant {
-            0 => Ok(TestVectorEvent::Transaction(d.decode_with(ctx)?, d.decode_with(ctx)?, d.decode_with(ctx)?)),
+            0 => Ok(TestVectorEvent::Transaction(
+                d.decode_with(ctx)?,
+                d.decode_with(ctx)?,
+                d.decode_with(ctx)?,
+            )),
             1 => Ok(TestVectorEvent::PassTick(d.decode_with(ctx)?)),
             2 => Ok(TestVectorEvent::PassEpoch(d.decode_with(ctx)?)),
             _ => Err(minicbor::decode::Error::message(
@@ -116,10 +109,19 @@ impl<'b, C> minicbor::decode::Decode<'b, C> for TestVectorEvent {
 // Traverse into the NewEpochState and find the utxos. In practice the initial ledger state for
 // each vector is very simple so we don't need to worry about every field. We really just care
 // about the utxos.
-fn decode_ledger_state<'b>(d: &mut minicbor::Decoder<'b>) -> Result<(DefaultValidationContext, &'b minicbor::bytes::ByteSlice, GovernanceActivity), minicbor::decode::Error> {
+fn decode_ledger_state<'b>(
+    d: &mut minicbor::Decoder<'b>,
+) -> Result<
+    (
+        DefaultValidationContext,
+        &'b minicbor::bytes::ByteSlice,
+        GovernanceActivity,
+    ),
+    minicbor::decode::Error,
+> {
     let _begin_nes = d.array()?;
     let _epoch_no = d.u64()?;
-    let _blocks_made  = d.skip()?;
+    let _blocks_made = d.skip()?;
     let _blocks_made = d.skip()?;
     let _begin_epoch_state = d.array()?;
     let _begin_account_state = d.skip()?;
@@ -143,17 +145,15 @@ fn decode_ledger_state<'b>(d: &mut minicbor::Decoder<'b>) -> Result<(DefaultVali
                 utxos_map.insert(tx_in, tx_out);
             }
         }
-        None => {
-            loop {
-                let ty = d.datatype()?;
-                if ty == minicbor::data::Type::Break {
-                    break;
-                }
-                let tx_in = d.decode()?;
-                let tx_out = d.decode()?;
-                utxos_map.insert(tx_in, tx_out);
+        None => loop {
+            let ty = d.datatype()?;
+            if ty == minicbor::data::Type::Break {
+                break;
             }
-        }
+            let tx_in = d.decode()?;
+            let tx_out = d.decode()?;
+            utxos_map.insert(tx_in, tx_out);
+        },
     }
     let _deposited = d.skip()?;
     let _fees = d.skip()?;
@@ -180,7 +180,7 @@ fn decode_ledger_state<'b>(d: &mut minicbor::Decoder<'b>) -> Result<(DefaultVali
         current_pparams_hash,
         GovernanceActivity {
             consecutive_dormant_epochs: u64::from(number_of_dormant_epochs) as u32,
-        }
+        },
     ))
 }
 
@@ -206,24 +206,21 @@ fn decode_segregated_parameters(
 
 fn import_vector(
     record: TestVector,
-    ledger_dir: &PathBuf,
     era_history: &EraHistory,
     pparams_dir: &PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let epoch = peek_epoch(&record.initial_state)?;
-
     let mut decoder = minicbor::Decoder::new(&record.initial_state);
-    let (mut validation_context, pparams_hash, governance_activity) = decode_ledger_state(&mut decoder)?;
+    let (mut validation_context, pparams_hash, governance_activity) =
+        decode_ledger_state(&mut decoder)?;
 
-    let protocol_parameters = decode_segregated_parameters(pparams_dir, &pparams_hash)?;
+    let protocol_parameters = decode_segregated_parameters(pparams_dir, pparams_hash)?;
 
     for (ix, event) in record.events.into_iter().enumerate() {
         let (tx_bytes, success, slot): (Bytes, bool, u64) = match event {
             TestVectorEvent::Transaction(tx, success, slot) => (tx, success, slot),
             _ => continue,
         };
-
-        let tx: MintedTx<'_> = minicbor::decode(&*tx_bytes)?;
+        let tx: MintedTx<'_> = minicbor::decode(tx_bytes.as_slice())?;
 
         let tx_body: KeepRaw<'_, MintedTransactionBody<'_>> = tx.transaction_body.clone();
         let tx_witness_set: MintedWitnessSet<'_> = tx.transaction_witness_set.deref().clone();
@@ -257,7 +254,7 @@ fn import_vector(
                 if success {
                     // Ok(())
                 } else {
-                    return Err(format!("Expected failure, got success").into());
+                    return Err("Expected failure, got success".into());
                 }
             }
             Err(e) => {
