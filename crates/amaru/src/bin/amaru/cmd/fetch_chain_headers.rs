b907eb7543d376802c8bc9371e9691cae4ec8769
jeluard <jeluard@users.noreply.github.com>
2025-11-21 11:33:24 +0100

    chore: update rust version
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- crates/amaru/src/bin/amaru/cmd/fetch_chain_headers.rs
+++ crates/amaru/src/bin/amaru/cmd/fetch_chain_headers.rs
@@ -16,7 +16,7 @@ use crate::cmd::{WorkerError, connect_to_peer};
 use amaru_kernel::{BlockHeader, IsHeader, Point, from_cbor, network::NetworkName, peer::Peer};
 use amaru_network::chain_sync_client::ChainSyncClient;
 use amaru_progress_bar::{ProgressBar, new_terminal_progress_bar};
-use clap::{Parser, arg};
+use clap::Parser;
 use pallas_network::miniprotocols::chainsync::{HeaderContent, NextResponse};
 use std::{
     error::Error,
