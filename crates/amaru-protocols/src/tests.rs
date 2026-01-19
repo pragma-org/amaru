dee1f9dfcea2183f981f2f2d67956cdd22d53dba
etorreborre <etorreborre@yahoo.com>
2026-01-19 15:34:56 +0100

    test: populate the initiator and responder chain stores to have one common starting header
    Signed-off-by: etorreborre <etorreborre@yahoo.com>

--- crates/amaru-protocols/src/tests.rs
+++ crates/amaru-protocols/src/tests.rs
@@ -12,11 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::manager;
+use crate::chainsync::ChainSyncInitiatorMsg;
 use crate::manager::{Manager, ManagerMessage};
+use crate::protocol::Role;
 use crate::store_effects::ResourceHeaderStore;
+use crate::{chainsync, manager};
+use amaru_kernel::is_header::tests::{any_headers_chain_with_root, make_header, run};
 use amaru_kernel::peer::Peer;
 use amaru_kernel::protocol_messages::network_magic::NetworkMagic;
+use amaru_kernel::{BlockHeader, HeaderHash, IsHeader};
 use amaru_mempool::InMemoryMempool;
 use amaru_network::connection::TokioConnections;
 use amaru_ouroboros_traits::can_validate_blocks::CanValidateHeaders;
@@ -25,17 +29,18 @@ use amaru_ouroboros_traits::can_validate_blocks::mock::{
 };
 use amaru_ouroboros_traits::in_memory_consensus_store::InMemConsensusStore;
 use amaru_ouroboros_traits::{
-    CanValidateBlocks, ConnectionId, ConnectionProvider, ConnectionResource, ResourceMempool,
+    CanValidateBlocks, ChainStore, ConnectionId, ConnectionProvider, ConnectionResource,
+    ResourceMempool,
 };
 use pallas_primitives::conway::Tx;
 use pure_stage::tokio::{TokioBuilder, TokioRunning};
 use pure_stage::{Effects, StageGraph, StageRef};
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::runtime::Handle;
 use tokio::time::timeout;
-use tracing::info;
 use tracing_subscriber::EnvFilter;
 
 #[tokio::test]
@@ -46,21 +51,27 @@ async fn connect_initiator() -> anyhow::Result<()> {
         .with_test_writer()
         .try_init();
 
-    info!("Creating the initiator");
+    tracing::info!("Creating the initiator");
     let peer = Peer::new(
         SocketAddr::from(([127, 0, 0, 1], 3005))
             .to_string()
             .as_str(),
     );
     let mut initiator_network = TokioBuilder::default();
-    let initiator_manager = Manager::new(NetworkMagic::PREPROD, StageRef::blackhole());
+    let chainsync = initiator_network.stage("chainsync", chainsync_stage);
+    let chainsync = initiator_network.wire_up(chainsync, ());
+    let initiator_manager = Manager::new(NetworkMagic::PREPROD, chainsync.without_state());
     let initiator_stage = initiator_network.stage("initiator", manager::stage);
     let initiator_stage = initiator_network.wire_up(initiator_stage, initiator_manager);
     let initiator_sender = initiator_network.input(initiator_stage);
     let initiator_connections = TokioConnections::new(65535);
-    add_resources(&mut initiator_network, initiator_connections);
+    add_resources(
+        &mut initiator_network,
+        Role::Initiator,
+        initiator_connections,
+    )?;
 
-    info!("Start the initiator");
+    tracing::info!("Start the initiator");
     let running_initiator = initiator_network.run(Handle::current());
     initiator_sender
         .send(ManagerMessage::AddPeer(peer.clone()))
@@ -71,6 +82,34 @@ async fn connect_initiator() -> anyhow::Result<()> {
     Ok(())
 }
 
+async fn chainsync_stage(_: (), msg: ChainSyncInitiatorMsg, eff: Effects<ChainSyncInitiatorMsg>) {
+    use crate::chainsync::InitiatorResult::*;
+    match msg.msg {
+        Initialize => {
+            tracing::info!(peer = %msg.peer,"initializing chainsync");
+        }
+        IntersectFound(point, tip) => {
+            tracing::info!(peer = %msg.peer, ?point, ?tip, "intersect found");
+        }
+        IntersectNotFound(tip) => {
+            tracing::info!(peer = %msg.peer, ?tip, "intersect not found");
+            eff.send(&msg.handler, chainsync::InitiatorMessage::Done)
+                .await;
+        }
+        RollForward(header_content, tip) => {
+            tracing::info!(peer = %msg.peer, variant = header_content.variant,
+                byron_prefix = ?header_content.byron_prefix, ?tip, "roll forward");
+            eff.send(&msg.handler, chainsync::InitiatorMessage::RequestNext)
+                .await;
+        }
+        RollBackward(point, tip) => {
+            tracing::info!(peer = %msg.peer, ?point, ?tip, "roll backward");
+            eff.send(&msg.handler, chainsync::InitiatorMessage::RequestNext)
+                .await;
+        }
+    }
+}
+
 #[tokio::test]
 #[ignore]
 async fn connect_responder() -> anyhow::Result<()> {
@@ -79,7 +118,7 @@ async fn connect_responder() -> anyhow::Result<()> {
         .with_test_writer()
         .try_init();
 
-    info!("Creating the responder");
+    tracing::info!("Creating the responder");
     let mut responder_network = TokioBuilder::default();
     let responder_manager = Manager::new(NetworkMagic::PREPROD, StageRef::blackhole());
     let responder_stage = responder_network.stage("responder", manager::stage);
@@ -93,10 +132,14 @@ async fn connect_responder() -> anyhow::Result<()> {
     let peer_addr = responder_connections
         .bind(SocketAddr::from(([127, 0, 0, 1], 3005)))
         .await?;
-    add_resources(&mut responder_network, responder_connections);
-    info!("Responder listening on {}", peer_addr);
+    add_resources(
+        &mut responder_network,
+        Role::Responder,
+        responder_connections,
+    )?;
+    tracing::info!("Responder listening on {}", peer_addr);
 
-    info!("Start the responder");
+    tracing::info!("Start the responder");
     let running_responder = responder_network.run(Handle::current());
     responder_sender.send(ManagerMessage::Accept).await.unwrap();
 
@@ -125,7 +168,7 @@ async fn wait_for(running_responder: TokioRunning, duration: Duration) -> anyhow
     match timeout(duration, running_responder.join()).await {
         Ok(_) => anyhow::bail!("test should have timed out"),
         Err(_) => {
-            info!("test timed out as expected");
+            tracing::info!("test timed out as expected");
         }
     };
     Ok(())
@@ -134,8 +177,13 @@ async fn wait_for(running_responder: TokioRunning, duration: Duration) -> anyhow
 pub type ResourceBlockValidation = Arc<dyn CanValidateBlocks + Send + Sync>;
 pub type ResourceHeaderValidation = Arc<dyn CanValidateHeaders + Send + Sync>;
 
-fn add_resources(network: &mut TokioBuilder, connections: TokioConnections) {
+fn add_resources(
+    network: &mut TokioBuilder,
+    role: Role,
+    connections: TokioConnections,
+) -> anyhow::Result<()> {
     let chain_store = Arc::new(InMemConsensusStore::default());
+    initialize_chain_store(chain_store.clone(), role)?;
     network
         .resources()
         .put::<ResourceHeaderStore>(chain_store.clone());
@@ -151,4 +199,28 @@ fn add_resources(network: &mut TokioBuilder, connections: TokioConnections) {
     network
         .resources()
         .put::<ResourceMempool<Tx>>(Arc::new(InMemoryMempool::default()));
+    Ok(())
+}
+
+fn initialize_chain_store(
+    chain_store: Arc<InMemConsensusStore<BlockHeader>>,
+    role: Role,
+) -> anyhow::Result<()> {
+    // Use the same root header for both initiator and responder
+    // but make the initiator chain shorter to force it to catch up to the responder
+    let origin_hash: HeaderHash = amaru_kernel::Hash::from_str(
+        "4df4505d862586f9e2c533c5fbb659f04402664db1b095aba969728abfb77301",
+    )?;
+    let root_header = BlockHeader::from(make_header(0, 0, Some(origin_hash)));
+    chain_store.set_anchor_hash(&root_header.hash())?;
+    let chain_size = if role == Role::Responder { 5 } else { 2 };
+    let mut headers = run(any_headers_chain_with_root(chain_size, root_header.hash()));
+    headers.insert(0, root_header);
+
+    for header in headers.iter() {
+        chain_store.store_header(&header)?;
+        chain_store.roll_forward_chain(&header.point())?;
+        chain_store.set_best_chain_hash(&header.hash())?;
+    }
+    Ok(())
 }
