e47abe0ddbb817c2f35ae995ad2afc676c91f781
yHSJ <josh@securitybot.info>
2026-06-26 13:28:33 -0500

    chore(test): update fixture schema to allow for more initial state, and introduce new fixtures
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-kernel/src/cardano/certificate_pointer.rs
+++ crates/amaru-kernel/src/cardano/certificate_pointer.rs
@@ -16,7 +16,8 @@ use std::fmt;
 
 use crate::{Slot, TransactionPointer, cbor};
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Default, PartialOrd)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Default, PartialOrd, serde::Serialize, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CertificatePointer {
     pub transaction: TransactionPointer,
     pub certificate_index: usize,
