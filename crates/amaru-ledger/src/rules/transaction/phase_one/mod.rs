4175b67f59a1bfd9e3c0d47fda7b1b3ac6ee5042
yHSJ <josh@securitybot.info>
2026-08-20 09:37:30 -0500

    chore: remove unused import
    Signed-off-by: yHSJ <josh@securitybot.info>

--- crates/amaru-ledger/src/rules/transaction/phase_one/mod.rs
+++ crates/amaru-ledger/src/rules/transaction/phase_one/mod.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{fmt, mem, ops::Deref, time::Instant};
+use std::{fmt, mem, time::Instant};
 
 use amaru_kernel::{
     AuxiliaryData, EraHistory, HasTransactionId, Hash, Lovelace, Network, NetworkName, ProtocolParameters,
